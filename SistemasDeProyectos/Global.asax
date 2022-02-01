cc3661594c746d55514d1eb43c8c5974e7dae552
Romero Ezequiel <romerorezequiel@gmail.com>
2022-02-01 13:35:31 -0300

    -

---
+++ SistemasDeProyectos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemasDeProyectos.MvcApplication" Language="C#" %>
---
+++ SistemasDeProyectos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemasDeProyectos.Startup))]
+namespace SistemasDeProyectos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
